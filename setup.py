e562f03aeb3f7ca3c835dffc2046e50b9c86cec0
unknown <22FOA@CHIX1C2-22FOA-1.BAIN.com>
2015-05-20 16:37:49 -0400

    added metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,21 @@ from setuptools import setup
 setup(
     name="minecart",
     version="0.1",
-    description="Simplified interface to pdfminer",
+    description="Pythonic parsing of PDFs",
     author="Felipe Ochoa",
+    author_email="find me through Github",
+    url="https://github.com/felipeochoa/minecart/issues",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='pdf pdfminer extract mining',
+    install_requires=['pdfminer'],
     packages=["minecart"],
     package_dir={'minecart': 'src'}
 )
