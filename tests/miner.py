5211a4aebdc820ba60c0ed902eb5702960c08d3c
unknown <22FOA@CHIX1C2-22FOA-1.BAIN.com>
2015-06-02 13:01:04 -0400

    added some unit tests for DeviceLoader

--- tests/miner.py
+++ tests/miner.py
@@ -7,11 +7,29 @@ import minecart.miner
 import minecart.color
 import pdfminer.pdfdevice
 
+def _patched_callable(obj):
+    "Monkeypatch to allow automocking of classmethods and staticmethods."
+    # See https://code.google.com/p/mock/issues/detail?id=241 and
+    # http://bugs.python.org/issue23078 for the relevant bugs this
+    # monkeypatch fixes
+    if isinstance(obj, type):
+        return True
+    if getattr(obj, '__call__', None) is not None:
+        return True
+    if (isinstance(obj, (staticmethod, classmethod)) and
+        mock._callable(obj.__func__)):  #pylint: disable=W0212
+        return True
+    return False
+
+mock._callable = _patched_callable  #pylint: disable=W0212
+
+
 class TestStrokeState(unittest.TestCase):
 
     "Testing of the StrokeState."
 
     def test_from_gs(self):
+        "Ensure StrokeState is created properly from a graphicstate."
         gs = minecart.miner.ColoredState()  #pylint: disable=C0103
         gs.linewidth = 3
         gs.linecap = 1
@@ -34,6 +52,7 @@ class TestFillState(unittest.TestCase):
     "Testing of the FillState"
 
     def test_from_gs(self):
+        "Ensure FillState is created properly from a graphicstate."
         gs = minecart.miner.ColoredState()  #pylint: disable=C0103
         gs.fill_color = color = object()
         fill = minecart.miner.FillState.from_gs(gs)
@@ -53,6 +72,7 @@ class TestColoredInterpreter(unittest.TestCase):
     @mock.patch('pdfminer.pdfinterp.PDFPageInterpreter.init_state',
                 autospec=True)
     def test_init_state(self, supermethod):
+        "Ensure the ColoredInterpreter uses a ColoredState."
         ctm = (1, 0, 0, 1, 0, 0)
         self.interp.init_state(ctm)
         supermethod.assert_called_once_with(self.interp, ctm)
@@ -72,7 +92,7 @@ class TestColoredInterpreter(unittest.TestCase):
 
     @mock.patch('pdfminer.pdfinterp.PDFPageInterpreter.init_resources',
                 autospec=True)
-    def test_init_resources_with_colormaps(self, supermethod):
+    def test_init_resources_colormaps(self, supermethod):
         "Test init_resources when there are color maps given."
         self.interp.init_resources({'ColorSpace': {
             'space1': ['DeviceRGB'],
@@ -89,6 +109,8 @@ class TestColoredInterpreter(unittest.TestCase):
 
 
 class TestColoredInterpreterGraphics(unittest.TestCase):
+
+    #pylint: disable=C0103
 
     "Test the graphics commands on the ColoredInterpreter."
 
@@ -220,8 +242,121 @@ class TestDeviceLoader(unittest.TestCase):
 
     "Test the DeviceLoader class."
 
+    def setUp(self):
+        self.device = minecart.miner.DeviceLoader(object())
+        self.device.page = mock.MagicMock(autospec=pdfminer.pdfpage.PDFPage)
+
+    def test_init(self):
+        "Test correct initialization of the device state."
+        device = minecart.miner.DeviceLoader(object())
+        self.assertIsNone(device.page)
+        self.assertIsNone(device.str_container)
+        self.assertEqual(device.unit, 1)
+
+    @mock.patch("minecart.miner.Page", autospec=True)
+    def test_begin_page(self, minecart_page):
+        "Ensure creation of a new page and updating of `.unit`."
+        page = mock.MagicMock(autospec=pdfminer.pdfpage.PDFPage)
+        page.attrs = {'UserUnit': 2}
+        self.device.begin_page(page, None)
+        minecart_page.assert_called_once_with(page)
+        self.assertEqual(self.device.unit, 2)
+
+    def test_set_ctm(self):
+        "Ensure the CTM responds to the `.unit` parameter."
+        ctm = (1, 1, 1, 2, 2, 2)
+        self.device.set_ctm(ctm)  # .unit is 1 from initialization
+        self.assertEqual(self.device.ctm, ctm)
+        self.device.unit = 2
+        self.device.set_ctm(ctm)
+        self.assertEqual(self.device.ctm, (2, 2, 2, 4, 4, 4))
+
+    @mock.patch("minecart.miner.Shape", autospec=True)
+    def test_paint_path_coordinates(self, minecart_shape):
+        "Test the coordinate conversion when painting a path."
+        user_path = [
+            ('m', 0, 0),
+            ('l', 5, 5),
+            ('c', 10, 10, 10, 7.5, 10, 5),
+            ('v', 15, 5, 20, 10),
+            ('y', 10, 20, 0, 10),
+            ('h',)
+            # pdfminer converts 're' to mlllh for us
+        ]
+        # The CTM is chosen to be a 90 degree counter-clockwise rotation,
+        # shifted by <5, 5>, with UserUnit set to 2
+        expected_path = [
+            ('m', 10, 10),
+            ('l', 0, 20),
+            ('c', -10, 30, -5, 30, 0, 30),
+            ('v', 0, 40, -10, 50),
+            ('y', -30, 30, -10, 10),
+            ('h',)
+        ]
+        self.device.unit = 2
+        self.device.set_ctm((0, 1, -1, 0, 5, 5))  # [TL, BL, TR, BR]
+        self.device.paint_path(object(), False, False, False, user_path)
+        minecart_shape.assert_called_once_with(None, None, False,
+                                               expected_path)
+
+    @mock.patch("minecart.miner.FillState.from_gs", autospec=True)
+    @mock.patch("minecart.miner.StrokeState.from_gs", autospec=True)
+    @mock.patch("minecart.miner.Shape", autospec=True)
+    def test_paint_path_graphics(self, shape, stroke_from_gs, fill_from_gs):
+        "Test correct passing of stroke/fill parameters to the Shape."
+        gstate = minecart.miner.ColoredState()
+        shape_obj = shape.return_value = object()
+        path = [('m', 10, 10), ('l', 20, 20), ('l', 30, 10), ('h',)]
+        self.device.ctm = (1, 0, 0, 1, 0, 0)
+
+        self.device.paint_path(gstate, True, False, False, path)
+        self.device.page.add_shape.assert_called_once_with(shape_obj)
+        shape.assert_called_once_with(stroke_from_gs.return_value, None,
+                                      False, path)
+        shape.reset_mock()
+        self.device.page.add_shape.reset_mock()
+        self.device.paint_path(gstate, False, True, False, path)
+        self.device.page.add_shape.assert_called_once_with(shape_obj)
+        shape.assert_called_once_with(None, fill_from_gs.return_value,
+                                      False, path)
+        shape.reset_mock()
+        self.device.page.add_shape.reset_mock()
+        self.device.paint_path(gstate, False, False, True, path)
+        self.device.page.add_shape.assert_called_once_with(shape_obj)
+        shape.assert_called_once_with(None, None, True, path)
+        shape.reset_mock()
+        self.device.page.add_shape.reset_mock()
+        self.device.paint_path(gstate, True, True, True, path)
+        self.device.page.add_shape.assert_called_once_with(shape_obj)
+        shape.assert_called_once_with(stroke_from_gs.return_value,
+                                      fill_from_gs.return_value,
+                                      True, path)
+
+    def test_paint_path_int(self):
+        "Test graphics params flowing through to Shape properly."
+        gstate = minecart.miner.ColoredState()
+        gstate.linewidth = 5
+        gstate.linecap = gstate.linejoin = 2
+        gstate.miterlimit = .5
+        gstate.dash = ([1, 1], 0)
+        gstate.stroke_color = minecart.color.DEVICE_RGB.make_color((0, 1, 1))
+        path = [('m', 10, 10), ('l', 20, 20), ('l', 30, 10), ('h',)]
+        self.device.page = mock.MagicMock(spec_set=minecart.miner.Page)
+        self.device.ctm = (1, 0, 0, 1, 0, 0)
+        self.device.paint_path(gstate, True, False, False, path)
+        self.device.page.add_shape.assert_called_once()
+        shape = self.device.page.add_shape.call_args[0][0]
+        self.assertEqual(shape.stroke.linewidth, 5)
+        self.assertEqual(shape.stroke.linecap, 2)
+        self.assertEqual(shape.stroke.linejoin, 2)
+        self.assertEqual(shape.stroke.miterlimit, .5)
+        self.assertEqual(shape.stroke.dash, ([1, 1], 0))
+        self.assertEqual(shape.stroke.color.value, (0, 1, 1))
+        self.assertIs(shape.stroke.color.space, minecart.color.DEVICE_RGB)
+        self.assertIsNone(shape.fill)
+
     def test_x(self):
-        self.fail("No test coverage of the DeviceLoader class!")
+        self.fail("Incomplete test coverage of the DeviceLoader class!")
 
 
 class TestDocument(unittest.TestCase):
